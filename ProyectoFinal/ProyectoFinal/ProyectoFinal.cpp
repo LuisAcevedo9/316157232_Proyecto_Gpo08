331441761641ce69a5ce78e2aefc3ad19c298085
Luis Acevedo <guicho3456@hotmail.com>
2022-03-30 12:25:17 -0600

    Avance 30/03/2022
    Añado el modelo de la lata (objeto 3)

--- ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
@@ -148,7 +148,7 @@ int main( )
     // Load textures
     //Model pokeArriba((char*)"Models/Pokeball/pokearriba.obj");
     //Model pokeAbajo((char*)"Models/Pokeball/pokeabajo.obj");
-    Model objetoProyecto((char*)"Models/ModelosProyecto/Bill/bill.obj");
+    Model objetoProyecto((char*)"Models/ModelosProyecto/lata/lata.obj");
 
 
     GLuint texture;
