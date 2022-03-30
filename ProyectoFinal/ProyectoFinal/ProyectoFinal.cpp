981aaf4aee2fc8f3e4700489814f35d84ad95a8a
Luis Acevedo <guicho3456@hotmail.com>
2022-03-30 13:26:21 -0600

    Avances 30/03/2022
    Añado el modelo del buro (objeto 7)

--- ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
+++ ProyectoFinal/ProyectoFinal/ProyectoFinal.cpp
@@ -148,7 +148,7 @@ int main( )
     // Load textures
     //Model pokeArriba((char*)"Models/Pokeball/pokearriba.obj");
     //Model pokeAbajo((char*)"Models/Pokeball/pokeabajo.obj");
-    Model objetoProyecto((char*)"Models/ModelosProyecto/mesa/mesa.obj");
+    Model objetoProyecto((char*)"Models/ModelosProyecto/Buro/buro.obj");
 
 
     GLuint texture;
